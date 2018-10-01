dea10fb8fc5d0d77df7dd3778a0d4ab3ca245eaf
Naoto Ikeno <ikenox@gmail.com>
2018-10-01 22:19:55 +0900

    update

--- src/comment-api-on-gae/app/handler.go
+++ src/comment-api-on-gae/app/handler.go
@@ -37,10 +37,6 @@ func NewEcho() engine.Handler {
 
 	e.Use(useAppEngine)
 
-	if env.IsProduction {
-		e.Use(middleware.Recover())
-	}
-
 	// TODO routingはどのレイヤの責務？
 	pc := controller.NewCommentController()
 	e.GET("/comment", pc.List)
